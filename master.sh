a74ec1fadb8d8ab5ec2c5ebcef4de1dbe3db813c
rehan6099 <103304908+rehan6099@users.noreply.github.com>
2022-08-10 21:16:59 +0530

    save

--- master.sh
+++ master.sh
@@ -4,3 +4,28 @@ bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environmen
 #slave
 sudo -i
 bash <(curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh)
+
+#jenkins
+#!/bin/bash
+echo ".........----------------#################..-.-Java and MAVEN-.-..#################----------------........."
+sudo apt install openjdk-8-jdk -y
+java -version
+sudo apt install -y maven
+mvn -v
+
+echo ".........----------------#################..-.-JENKINS-.-..#################----------------........."
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y jenkins
+systemctl daemon-reload
+systemctl enable jenkins
+sudo systemctl start jenkins
+#sudo systemctl status jenkins
+sudo usermod -a -G docker jenkins
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+echo ".........----------------#################..-.-COMPLETED-.-..#################----------------........."
+
+
+#also install docker and kubectl on jenkins machine
